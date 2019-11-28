97bdc4fbc43eb4fad8527dd0a720ca1693585eae
hyo-jin-jeong <gaya309@naver.com>
2019-11-28 21:36:01 +0900

    11/28 Calendar.kt calendar.xml change, emotion_select.xml add_schedule.xml add,Putpicture.kt delete

--- MyDiary/app/src/main/java/com/example/mydiary/Calendar.kt
+++ MyDiary/app/src/main/java/com/example/mydiary/Calendar.kt
@@ -2,7 +2,6 @@ package com.example.mydiary
 
 import android.annotation.SuppressLint
 import android.content.Context.MODE_NO_LOCALIZED_COLLATORS
-import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,15 +9,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
 import android.widget.Toast
-import androidx.annotation.RequiresPermission
-import androidx.appcompat.app.AppCompatActivity
+
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.add_schedule.*
+import kotlinx.android.synthetic.main.add_schedule.view.*
 import kotlinx.android.synthetic.main.calendar.*
 import kotlinx.android.synthetic.main.calendar.view.*
-import java.io.File
+import kotlinx.android.synthetic.main.emotion_select.*
 import java.io.FileInputStream
 import java.io.FileOutputStream
-import java.util.Calendar
 
 class Calendar : Fragment() {
 
@@ -31,109 +30,120 @@ class Calendar : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
+//        val appendDay = inflater.inflate(R.layout.add_schedule,container,false)
+//        container?.calendarLayout?.addView(appendDay)
+        //val inf = schedule
+
+        //emotionLayout.visibility = View.INVISIBLE
         val inf = inflater.inflate(R.layout.calendar, container, false)
-        inf.calendarView.setOnDayClickListener{
+        with(inf) {
+            schedule.visibility = View.VISIBLE
+            calendarView?.setOnDateChangeListener { view, year, month, dayOfMonth ->
+                diaryTextView.visibility = View.INVISIBLE
+                save_Btn.visibility = View.INVISIBLE
+                cha_Btn.visibility = View.INVISIBLE
+                del_Btn.visibility = View.INVISIBLE
+                contextEditText.visibility = View.INVISIBLE
+                textView2.visibility = View.INVISIBLE
+                var month = month + 1
 
-            var date =it.calendar.time.toString().split(" ")
-            var year = date[date.size-1].toInt()
-            var month_temp = date[1]
-            var dayOfMonth=date[2].toInt()
-            var month:Int=0
-            when(month_temp){
-                "Jan"-> month=1
-                "Feb" -> month =2
-                "Mar"->month=3
-                "Apr"->month=4
-                "May"->month=5
-                "Jun"->month=6
-                "Jul"->month=7
-                "Aug"->month=8
-                "Sept"->month=9
-                "Oct"->month=10
-                "Nov"->month=11
-                "Dec"->month=12
-            }
+                val msg: String =
+                    year.toString() + "/" + month.toString() + "/" + dayOfMonth.toString()
 
-             val msg =  year.toString() + "/" + month.toString() + "/" + dayOfMonth.toString()
+                val popupMenu: PopupMenu = PopupMenu(getContext(), view)
+                popupMenu.menuInflater.inflate(R.menu.option_menu, popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
 
-            val popupMenu: PopupMenu = PopupMenu(getContext(), view)
-            popupMenu.menuInflater.inflate(R.menu.option_menu, popupMenu.menu)
-            popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
-                when (item.itemId) {
-                    R.id.schedule -> {
-                        Toast.makeText(
-                            getContext(),
-                            "You Clicked : " + item.title,
-                            Toast.LENGTH_SHORT
-                        ).show()
+                    when (item.itemId) {
+                        R.id.schedule ->{
 
-                        //달력 날짜 선택 후 일정 버튼을 누르면
-                        calendarTextView.visibility = View.VISIBLE //해당 날짜가 뜨는 textView
-                        save_Btn.visibility = View.VISIBLE  //저장 버튼이 Visible
-                        contextEditText.visibility = View.VISIBLE //EditText가 Visible
-                        textView2.visibility = View.INVISIBLE //저장된 textView가 invisible
-                        cha_Btn.visibility = View.INVISIBLE //수정 버튼이 invisible
-                        del_Btn.visibility = View.INVISIBLE //삭제 버튼이 invisible
+                            Toast.makeText(
+                                getContext(),
+                                "You Clicked : " + item.title,
+                                Toast.LENGTH_SHORT
+                            ).show()
 
-                        diaryTextView.text =
-                            String.format("%d / %d / %d", year, month + 1, dayOfMonth)
+                            //달력 날짜 선택 후 일정 버튼을 누르면
+                            with(schedule) {
+                                visibility = View.VISIBLE
+                                diaryTextView.visibility = View.VISIBLE //해당 날짜가 뜨는 textView
+                                save_Btn.visibility = View.VISIBLE  //저장 버튼이 Visible
+                                contextEditText.visibility = View.VISIBLE //EditText가 Visible
+                                textView2.visibility = View.INVISIBLE //저장된 textView가 invisible
+                                cha_Btn.visibility = View.INVISIBLE //수정 버튼이 invisible
+                                del_Btn.visibility = View.INVISIBLE //삭제 버튼이 invisible
+
+                                diaryTextView.text = String.format("%d / %d / %d", year, month , dayOfMonth)
 //날짜를 보여주는 텍스트에 해달 날짜를 넣는다.
-                        contextEditText.setText("")//EditText에 공백값 넣
+                                contextEditText.setText("")//EditText에 공백값 넣
 
-                        checkedDay(year, month, dayOfMonth) //checkedDay 메소드 호출
-                    }
+                                checkedDay(year, month, dayOfMonth) //checkedDay 메소드 호출
+                            }
+                            emotion.visibility = View.GONE
+                        }
 
-                    R.id.diary -> {
-                        Toast.makeText(
-                            getContext(),
-                            "You Clicked : " + item.title,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        val i = Intent(getContext(), WriteDiary::class.java)
-                        i.putExtra("date", msg)
-                        startActivity(i)
+                        R.id.diary -> {
+                            Toast.makeText(
+                                getContext(),
+                                "You Clicked : " + item.title,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            val i = Intent(getContext(), WriteDiary::class.java)
+                            i.putExtra("date", msg)
+                            startActivity(i)
+                        }
+                        R.id.emotion ->{
+                            Toast.makeText(
+                                getContext(),
+                                "You Clicked : " + item.title,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            emotion.visibility = View.VISIBLE
+                            schedule.visibility = View.GONE
+//                            val intent = Intent(getContext(), EmotionSe::class.java)
+//                            startActivity(intent)
+                        }
                     }
-                    R.id.emotion ->{
-                        Toast.makeText(
-                            getContext(),
-                            "You Clicked : " + item.title,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        val intent = Intent(getContext(), EmotionSe::class.java)
-                        startActivity(intent)
+                    true
+                })
+
+
+                with(schedule) {
+                    diaryTextView.visibility = View.INVISIBLE
+                    save_Btn.visibility = View.INVISIBLE
+                    contextEditText.visibility = View.INVISIBLE
+                    popupMenu.show()
+                    Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show() //날짜 터치시 알림표시로 나타내기
+
+                    save_Btn.setOnClickListener {
+                        //저장 버튼이 클릭되면
+                        saveDiary(fname) //saveDiary 메소드 호출
+                        Toast.makeText(activity, fname + "데이터를 저장했습니다.", Toast.LENGTH_LONG).show()
+                        str = contextEditText.getText().toString() // str 변수에 edittext내용을 toString
+//형으로 저장
+                        textView2.text = "${str}" // textView에 str 출력
+                        save_Btn.visibility = View.INVISIBLE
+                        cha_Btn.visibility = View.VISIBLE
+                        del_Btn.visibility = View.VISIBLE
+                        contextEditText.visibility = View.INVISIBLE
+                        textView2.visibility = View.VISIBLE
+
+
                     }
                 }
-                true
-            })
-
-
-            calendarTextView.visibility = View.INVISIBLE
-            save_Btn.visibility = View.INVISIBLE
-            contextEditText.visibility = View.INVISIBLE
-            popupMenu.show()
-            Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show() //날짜 터치시 알림표시로 나타내기
-
-            save_Btn.setOnClickListener {
-                //저장 버튼이 클릭되면
-                saveDiary(fname) //saveDiary 메소드 호출
-                Toast.makeText(activity, fname + "데이터를 저장했습니다.", Toast.LENGTH_LONG).show()
-                str = contextEditText.getText().toString() // str 변수에 edittext내용을 toString
-//형으로 저장
-                textView2.text = "${str}" // textView에 str 출력
-                save_Btn.visibility = View.INVISIBLE
-                cha_Btn.visibility = View.VISIBLE
-                del_Btn.visibility = View.VISIBLE
-                contextEditText.visibility = View.INVISIBLE
-                textView2.visibility = View.VISIBLE
 
             }
-
         }
+
+
 
         return inf
     }
 
     fun checkedDay(cYear: Int, cMonth: Int, cDay: Int) {
+
         fname = "" + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt"
 // 저장할 파일 이름 설정. Ex) 2019-01-20.txt
         var fis: FileInputStream? = null // FileStream fis 변수 설정
@@ -179,7 +189,7 @@ class Calendar : Fragment() {
                 cha_Btn.visibility = View.INVISIBLE
                 del_Btn.visibility = View.INVISIBLE
                 removeDiary(fname)
-                Toast.makeText(activity, fname + "데이터를 삭제했습니다.", Toast.LENGTH_LONG).show()
+                Toast.makeText(activity,fname + "데이터를 삭제했습니다.",Toast.LENGTH_LONG).show()
             }
 
             if (textView2.getText() == "") {
@@ -202,10 +212,7 @@ class Calendar : Fragment() {
         var fos: FileOutputStream? = null
 
         try {
-            fos = activity?.openFileOutput(
-                readyDay,
-                MODE_NO_LOCALIZED_COLLATORS
-            ) // MODE_NO_LOCALIZED_COLLATORS
+            fos = activity?.openFileOutput(readyDay,MODE_NO_LOCALIZED_COLLATORS) // MODE_NO_LOCALIZED_COLLATORS
             var content: String = contextEditText.getText().toString()
             fos?.write(content.toByteArray())
             fos?.close()
