0889b07d9083d62c5688cbe59d0b1f3f29e73ad6
hyo-jin-jeong <gaya309@naver.com>
2019-12-03 17:15:38 +0900

    12/3 ui change

--- MyDiary/app/src/main/java/com/example/mydiary/Calendar.kt
+++ MyDiary/app/src/main/java/com/example/mydiary/Calendar.kt
@@ -2,7 +2,6 @@ package com.example.mydiary
 
 import android.annotation.SuppressLint
 import android.content.Context.MODE_NO_LOCALIZED_COLLATORS
-import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,216 +9,236 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
 import android.widget.Toast
-import androidx.annotation.RequiresPermission
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.add_schedule.*
+
+
+import kotlinx.android.synthetic.main.add_schedule.view.*
 import kotlinx.android.synthetic.main.calendar.*
+
+
 import kotlinx.android.synthetic.main.calendar.view.*
-import java.io.File
-import java.io.FileInputStream
+import kotlinx.android.synthetic.main.emotion_select.*
+
+
 import java.io.FileOutputStream
-import java.util.Calendar
+import java.util.*
+
+data class Schedule(
+    var comment : String = ""
+)
 
 class Calendar : Fragment() {
-
+    lateinit var root: DatabaseReference
+    var name = " "
     var fname: String = ""
     var str: String = ""
 
+    lateinit var rootT: DatabaseReference
+    lateinit var txtRef: DatabaseReference
+
+    lateinit var SchRef: DatabaseReference
+
+    var comment: String = ""
+
+
+    var year = 0
+    var month = 0
+    var day = 0
     @SuppressLint("ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
+        rootT = FirebaseDatabase.getInstance().reference.child(UserModel.uid)
+        txtRef = FirebaseDatabase.getInstance().reference.child("text")
+
         val inf = inflater.inflate(R.layout.calendar, container, false)
-        inf.calendarView.setOnDayClickListener{
 
-            var date =it.calendar.time.toString().split(" ")
-            var year = date[date.size-1].toInt()
-            var month_temp = date[1]
-            var dayOfMonth=date[2].toInt()
-            var month:Int=0
-            when(month_temp){
-                "Jan"-> month=1
-                "Feb" -> month =2
-                "Mar"->month=3
-                "Apr"->month=4
-                "May"->month=5
-                "Jun"->month=6
-                "Jul"->month=7
-                "Aug"->month=8
-                "Sept"->month=9
-                "Oct"->month=10
-                "Nov"->month=11
-                "Dec"->month=12
-            }
+        with(inf) {
+            textMsg.setSelected(true)
+            nextTXT.setOnClickListener {
 
-             val msg =  year.toString() + "/" + month.toString() + "/" + dayOfMonth.toString()
-
-            val popupMenu: PopupMenu = PopupMenu(getContext(), view)
-            popupMenu.menuInflater.inflate(R.menu.option_menu, popupMenu.menu)
-            popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
-                when (item.itemId) {
-                    R.id.schedule -> {
-                        Toast.makeText(
-                            getContext(),
-                            "You Clicked : " + item.title,
-                            Toast.LENGTH_SHORT
-                        ).show()
-
-                        //달력 날짜 선택 후 일정 버튼을 누르면
-                        calendarTextView.visibility = View.VISIBLE //해당 날짜가 뜨는 textView
-                        save_Btn.visibility = View.VISIBLE  //저장 버튼이 Visible
-                        contextEditText.visibility = View.VISIBLE //EditText가 Visible
-                        textView2.visibility = View.INVISIBLE //저장된 textView가 invisible
-                        cha_Btn.visibility = View.INVISIBLE //수정 버튼이 invisible
-                        del_Btn.visibility = View.INVISIBLE //삭제 버튼이 invisible
-
-                        diaryTextView.text =
-                            String.format("%d / %d / %d", year, month + 1, dayOfMonth)
-//날짜를 보여주는 텍스트에 해달 날짜를 넣는다.
-                        contextEditText.setText("")//EditText에 공백값 넣
-
-                        checkedDay(year, month, dayOfMonth) //checkedDay 메소드 호출
+                var listener = object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError) {
                     }
 
-                    R.id.diary -> {
-                        Toast.makeText(
-                            getContext(),
-                            "You Clicked : " + item.title,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        val i = Intent(getContext(), WriteDiary::class.java)
-                        i.putExtra("date", msg)
-                        startActivity(i)
-                    }
-                    R.id.emotion ->{
-                        Toast.makeText(
-                            getContext(),
-                            "You Clicked : " + item.title,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        val intent = Intent(getContext(), EmotionSe::class.java)
-                        startActivity(intent)
+                    override fun onDataChange(p1: DataSnapshot) {
+                        val random = Random()
+                        val num = random.nextInt(5)
+                        val arr = arrayOfNulls<String>(5)
+                        var i = 0
+                        for (text in p1.children) {
+                            arr[i] = text.value.toString()
+                            i++
+                        }
+                        textMsg.setText(arr[num])
                     }
                 }
-                true
-            })
+                txtRef.addValueEventListener(listener)
+            }
+            schedule.visibility = View.VISIBLE
 
 
-            calendarTextView.visibility = View.INVISIBLE
-            save_Btn.visibility = View.INVISIBLE
-            contextEditText.visibility = View.INVISIBLE
-            popupMenu.show()
-            Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show() //날짜 터치시 알림표시로 나타내기
+            calendarView?.setOnDateChangeListener { view, y, m, d ->
+                emotion.visibility = View.GONE
+                schedule.visibility = View.GONE
 
-            save_Btn.setOnClickListener {
-                //저장 버튼이 클릭되면
-                saveDiary(fname) //saveDiary 메소드 호출
-                Toast.makeText(activity, fname + "데이터를 저장했습니다.", Toast.LENGTH_LONG).show()
-                str = contextEditText.getText().toString() // str 변수에 edittext내용을 toString
-//형으로 저장
-                textView2.text = "${str}" // textView에 str 출력
-                save_Btn.visibility = View.INVISIBLE
-                cha_Btn.visibility = View.VISIBLE
-                del_Btn.visibility = View.VISIBLE
-                contextEditText.visibility = View.INVISIBLE
-                textView2.visibility = View.VISIBLE
+                month = m + 1
+                year = y
+                day = d
+                val msg: String =
+                    year.toString() + "/" + month.toString() + "/" + day.toString()
+
+                val popupMenu = PopupMenu(getContext(), view)
+                popupMenu.menuInflater.inflate(R.menu.option_menu, popupMenu.menu)
+                popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+
+                    when (item.itemId) {
+                        R.id.schedule -> {
+                            //달력 날짜 선택 후 일정 버튼을 누르면
+                            scheduleCheck()
+                        }
+                        R.id.diary -> {
+                            val i = Intent(getContext(), WriteDiary::class.java)
+                            i.putExtra("date", msg)
+                            startActivity(i)
+                        }
+                        R.id.emotion -> {
+                            Toast.makeText(
+                                getContext(),
+                                "You Clicked : " + item.title,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            emotion.visibility = View.VISIBLE
+                            schedule.visibility = View.GONE
+
+                            emotionSelect()
+
+                        }
+                    }
+                    true
+                })
+
+                with(schedule) {
+                    diaryTextView.visibility = View.INVISIBLE
+                    save_Btn.visibility = View.INVISIBLE
+                    contextEditText.visibility = View.INVISIBLE
+                    popupMenu.show()
+                    Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show() //날짜 터치시 알림표시로 나타내기
+
+                    save_Btn.setOnClickListener {
+                        //저장 버튼이 클릭되면
+                        saveDiary(fname) //saveDiary 메소드 호출
+                        Toast.makeText(activity, fname + "데이터를 저장했습니다.", Toast.LENGTH_LONG)
+                            .show()
+                        str = contextEditText.getText()
+                            .toString() // str 변수에 edittext내용을 toString 형으로 저장
+                        textView2.text = "${str}" // textView에 str 출력
+                        save_Btn.visibility = View.INVISIBLE
+                        cha_Btn.visibility = View.VISIBLE
+                        del_Btn.visibility = View.VISIBLE
+                        contextEditText.visibility = View.INVISIBLE
+                        textView2.visibility = View.VISIBLE
+
+                    }
+                    cha_Btn.setOnClickListener {
+                        // 수정 버튼을 누를 시
+                        contextEditText.visibility = View.VISIBLE
+                        textView2.visibility = View.INVISIBLE
+                        contextEditText.setText(str) // editText에 textView에 저장된 내용을 출력
+                        save_Btn.visibility = View.VISIBLE
+                        cha_Btn.visibility = View.INVISIBLE
+                        del_Btn.visibility = View.INVISIBLE
+                        textView2.text = "${contextEditText.getText()}"
+                    }
+
+                    del_Btn.setOnClickListener {
+                        textView2.visibility = View.INVISIBLE
+                        contextEditText.setText("")
+                        contextEditText.visibility = View.VISIBLE
+                        save_Btn.visibility = View.VISIBLE
+                        cha_Btn.visibility = View.INVISIBLE
+                        del_Btn.visibility = View.INVISIBLE
+                        removeDiary(fname)
+                        Toast.makeText(activity, fname + "데이터를 삭제했습니다.", Toast.LENGTH_LONG).show()
+                    }
+
+                }
 
             }
-
         }
-
         return inf
     }
 
-    fun checkedDay(cYear: Int, cMonth: Int, cDay: Int) {
-        fname = "" + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt"
-// 저장할 파일 이름 설정. Ex) 2019-01-20.txt
-        var fis: FileInputStream? = null // FileStream fis 변수 설정
+    fun scheduleCheck(){
+        val sche=rootT.child("Schedule").child("$year$month").child(day.toString())
+        sche.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                schedule.visibility = View.VISIBLE
+                if(p0.getValue()!=null){
+                    val attach_txt=p0.getValue().toString()
+                    contextEditText.setText(attach_txt)
+                    with(schedule){
 
-        try {
-            fis = activity?.openFileInput(fname) // fname 파일 오픈!!
+                        save_Btn.visibility = View.INVISIBLE  //저장 버튼이 Visible
+                        cha_Btn.visibility = View.VISIBLE //수정 버튼이 invisible
+                        del_Btn.visibility = View.VISIBLE //삭제 버튼이 invisible
+                    }
 
-            val fileData = fis?.available()?.let { ByteArray(it) } // fileData에 파이트 형식
-//으로 저장
+                }
+                else{
+                    with(schedule){
 
-            fis?.read(fileData)
-// fileData를 읽음
-            fis?.close()
+                        save_Btn.visibility = View.VISIBLE  //저장 버튼이 Visible
+                        cha_Btn.visibility = View.INVISIBLE //수정 버튼이 invisible
+                        del_Btn.visibility = View.INVISIBLE //삭제 버튼이 invisible
 
+                    }
 
-            str = fileData?.let { String(it) }.toString() // str 변수에 fileData를 저장
-
-            contextEditText.visibility = View.INVISIBLE
-            textView2.visibility = View.VISIBLE
-            textView2.text = "${str}" // textView에 str 출력
-
-            save_Btn.visibility = View.INVISIBLE
-            cha_Btn.visibility = View.VISIBLE
-            del_Btn.visibility = View.VISIBLE
-
-            cha_Btn.setOnClickListener {
-                // 수정 버튼을 누를 시
-                contextEditText.visibility = View.VISIBLE
-                textView2.visibility = View.INVISIBLE
-                contextEditText.setText(str) // editText에 textView에 저장된
-// 내용을 출력
-                save_Btn.visibility = View.VISIBLE
-                cha_Btn.visibility = View.INVISIBLE
-                del_Btn.visibility = View.INVISIBLE
-                textView2.text = "${contextEditText.getText()}"
+                }
             }
-
-            del_Btn.setOnClickListener {
-                textView2.visibility = View.INVISIBLE
-                contextEditText.setText("")
-                contextEditText.visibility = View.VISIBLE
-                save_Btn.visibility = View.VISIBLE
-                cha_Btn.visibility = View.INVISIBLE
-                del_Btn.visibility = View.INVISIBLE
-                removeDiary(fname)
-                Toast.makeText(activity, fname + "데이터를 삭제했습니다.", Toast.LENGTH_LONG).show()
+            override fun onCancelled(p0: DatabaseError) {
             }
+        })
 
-            if (textView2.getText() == "") {
-                textView2.visibility = View.INVISIBLE
-                diaryTextView.visibility = View.VISIBLE
-                save_Btn.visibility = View.VISIBLE
-                cha_Btn.visibility = View.INVISIBLE
-                del_Btn.visibility = View.INVISIBLE
-                contextEditText.visibility = View.VISIBLE
-            }
+        with(schedule){
+            diaryTextView.visibility = View.VISIBLE //해당 날짜가 뜨는 textView
 
-        } catch (e: Exception) {
-            e.printStackTrace()
+            contextEditText.visibility = View.VISIBLE //EditText가 Visible
+            textView2.visibility = View.INVISIBLE //저장된 textView가 invisible
+
+            diaryTextView.text = String.format(
+                "%d / %d / %d",
+                year,
+                month,
+                day
+            ) //날짜를 보여주는 텍스트에 해달 날짜를 넣는다.
+            contextEditText.setText("")//EditText에 공백값 넣기
         }
+        emotion.visibility = View.GONE
     }
-
 
     @SuppressLint("WrongConstant")
     fun saveDiary(readyDay: String) {
-        var fos: FileOutputStream? = null
 
-        try {
-            fos = activity?.openFileOutput(
-                readyDay,
-                MODE_NO_LOCALIZED_COLLATORS
-            ) // MODE_NO_LOCALIZED_COLLATORS
-            var content: String = contextEditText.getText().toString()
-            fos?.write(content.toByteArray())
-            fos?.close()
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
+        var dataInput=  Schedule(contextEditText.text.toString())
+        var dR = rootT.child("Schedule").child("$year$month").child(day.toString())
+        val text =dR.setValue(contextEditText.text.toString())
+        contextEditText.text.clear()
+        contextEditText.text.append(comment)
     }
 
     @SuppressLint("WrongConstant")
     fun removeDiary(readyDay: String) {
-        var fos: FileOutputStream? = null
 
+        SchRef = rootT.child("Schedule").child("$year$month").child(day.toString())
+        SchRef.removeValue()
+        var fos: FileOutputStream? = null
         try {
             fos = activity?.openFileOutput(readyDay, MODE_NO_LOCALIZED_COLLATORS)//MODE_PRIVATE
             var content: String = ""
@@ -229,7 +248,50 @@ class Calendar : Fragment() {
         } catch (e: Exception) {
             e.printStackTrace()
         }
+    }
 
+    fun emotionSelect() {
+        root = FirebaseDatabase.getInstance().reference.child(UserModel.uid)
 
+        imageButton.setOnClickListener {
+            name = "angry"
+            Change(name)
+
+            Toast.makeText(getContext(), "angry", Toast.LENGTH_SHORT).show()
+
+        }
+        imageButton2.setOnClickListener {
+            name = "sad"
+            Change(name)
+
+            Toast.makeText(getContext(), "sad", Toast.LENGTH_SHORT).show()
+
+        }
+        imageButton3.setOnClickListener {
+            name = "confused"
+            Change(name)
+
+            Toast.makeText(getContext(), "confused", Toast.LENGTH_SHORT).show()
+
+        }
+        imageButton4.setOnClickListener {
+            name = "happy"
+            Change(name)
+
+            Toast.makeText(getContext(), "happy", Toast.LENGTH_SHORT).show()
+
+        }
+        imageButton5.setOnClickListener {
+            name = "love"
+            Change(name)
+
+            Toast.makeText(getContext(), "love", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun Change(name: String) {
+        val emotionRef = root.child("Diary").child("$year$month").child("$day").child("emotion")
+        emotionRef.setValue(name)
     }
 }
--- MyDiary/app/src/main/java/com/example/mydiary/Emotion.kt
+++ MyDiary/app/src/main/java/com/example/mydiary/Emotion.kt
@@ -1,53 +1,144 @@
 package com.example.mydiary
 
-
-import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.components.Description
 
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
-import kotlinx.android.synthetic.main.emotion_result.*
+import com.google.firebase.database.*
+
 import kotlinx.android.synthetic.main.emotion_result.view.*
+import kotlinx.android.synthetic.main.emotion_result.view.chart_spinner_m
+import kotlinx.android.synthetic.main.emotion_result.view.chart_spinner_y
+import kotlinx.android.synthetic.main.emotion_result.view.search_chart as search_chart1
 
 
 class Emotion : Fragment() {
+    lateinit var root: DatabaseReference
+    lateinit var yearSpinner: Spinner
+    lateinit var monthSpinner: Spinner
+    lateinit var yearAdapter : Adapter
+    lateinit var monthAdapter : Adapter
+    lateinit var inf:View
+    var year:String=""
+    var yearMonth:String = ""
 
+    var emotionSave= ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val a=inflater.inflate(R.layout.emotion_result, container, false)
-                a.pieChart.setUsePercentValues(true)
-        a.pieChart.getDescription().setEnabled(false)
-        a.pieChart.setExtraOffsets(5F, 10F, 5F, 5F)
+        inf = inflater.inflate(R.layout.emotion_result, container, false)
+        yearSpinner = inf.chart_spinner_y
+        yearAdapter = ArrayAdapter.createFromResource(
+            context,
+            R.array.date_year,
+            android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            yearSpinner.adapter = adapter
+        }
 
-        a.pieChart.setDragDecelerationFrictionCoef(0.95f)
+        yearSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when(position) {
+                    1 -> year = 2016.toString()
+                    2 -> year = 2017.toString()
+                    3 -> year = 2018.toString()
+                    4 -> year = 2019.toString()
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
 
-        a.pieChart.setDrawHoleEnabled(false)
-        a.pieChart.setHoleColor(Color.WHITE)
-        a.pieChart.setTransparentCircleRadius(61f)
+            }
+        }
+        monthSpinner = inf.chart_spinner_m
+        monthAdapter = ArrayAdapter.createFromResource(
+            context,
+            R.array.date_month,
+            android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            monthSpinner.adapter = adapter
+        }
+        monthSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                yearMonth=year+position.toString()
+            }
 
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+        inf.search_chart1.setOnClickListener {
+            emotionResult(yearMonth)
+
+            Log.d(tag,yearMonth)
+            Toast.makeText(getContext(),yearMonth,Toast.LENGTH_LONG)
+        }
+
+        return inf
+    }
+    private fun emotionResult(y: String) {
+        root = FirebaseDatabase.getInstance().reference.child(UserModel.uid).child("Diary").child(y)
+        root.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+            override fun onDataChange(p: DataSnapshot) {
+                EmotionModel.emotionInit()
+                for (filesnapshot in p.getChildren()) {
+                    emotionSave = filesnapshot.child("emotion").value.toString()
+                    when (emotionSave) {
+                        "angry" -> EmotionModel.angry++
+                        "confused" -> EmotionModel.confused++
+                        "happy" -> EmotionModel.happy++
+                        "love" -> EmotionModel.love++
+                        "sad" -> EmotionModel.sad++
+                    }
+
+                }
+                chartUpdate()
+
+            }
+        })
+
+    }
+    private fun chartUpdate(){
+        with(inf) {
+            pieChart.setUsePercentValues(true)
+            pieChart.getDescription().setEnabled(false)
+            pieChart.setExtraOffsets(5F, 10F, 5F, 5F)
+
+            pieChart.setDragDecelerationFrictionCoef(0.95f)
+
+            pieChart.setDrawHoleEnabled(false)
+            pieChart.setHoleColor(android.graphics.Color.WHITE)
+            pieChart.setTransparentCircleRadius(61f)
+        }
         val yValues = ArrayList<PieEntry>()
+        if(!EmotionModel.angry.equals(0F))
+            yValues.add(PieEntry(EmotionModel.angry, "angry"))
+        if(!EmotionModel.sad.equals(0F))
+            yValues.add(PieEntry(EmotionModel.sad, "sad"))
+        if(!EmotionModel.confused.equals(0F))
+            yValues.add(PieEntry(EmotionModel.confused, "confused"))
+        if(!EmotionModel.happy.equals(0F))
+            yValues.add(PieEntry(EmotionModel.happy, "happy"))
+        if(!EmotionModel.love.equals(0F))
+            yValues.add(PieEntry(EmotionModel.love, "love"))
 
-        yValues.add(PieEntry(34f, "1"))
-//        yValues.add(PieEntry(23f, "2"))
-//        yValues.add(PieEntry(14f, "3"))
-//        yValues.add(PieEntry(35f, "4"))
-//        yValues.add(PieEntry(40f, "5"))
 
-
-
-
-        a.pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic) //애니메이션
+        inf.pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic) //애니메이션
 
         val dataSet = PieDataSet(yValues, "기분 변화")
         dataSet.sliceSpace = 3f
@@ -58,15 +149,7 @@ class Emotion : Fragment() {
         data.setValueTextSize(10f)
         data.setValueTextColor(Color.YELLOW)
 
-        a.pieChart.setData(data)
-        a.emo_select.setOnClickListener { v: View ->
-            val intent = Intent(getContext(), EmotionSe::class.java)
-            startActivity(intent)
-        }
-        return a
-
-
+        inf.pieChart.setData(data)
     }
-
 }
 
