d5b4d64494de365f39e0be2bd7c6521c0a7f370c
panda981018 <panda981018@gmail.com>
2019-12-03 16:10:15 +0900

    12/3 Create hash_item.xml, HashListAdapter.kt, and changing diary_list.xml, WriteDiary.kt, and DiaryList.kt

--- MyDiary/app/src/main/java/com/example/mydiary/DiaryList.kt
+++ MyDiary/app/src/main/java/com/example/mydiary/DiaryList.kt
@@ -2,6 +2,7 @@ package com.example.mydiary
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,14 +11,20 @@ import androidx.fragment.app.Fragment
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.diary_list.view.*
 
+data class Hash(val hashtag:String, val url:String)
 class DiaryList : Fragment(), AdapterView.OnItemSelectedListener {
     @SuppressLint("ResourceType")
     lateinit var root:DatabaseReference
+    lateinit var hashRef:DatabaseReference // 해시태그 레퍼런스(Picutre 아래의 Hash를 가리킴)
     lateinit var yearSpinner:Spinner
     lateinit var monthSpinner:Spinner
     lateinit var yearAdapter : Adapter
     lateinit var monthAdapter : Adapter
     var yearMonth:String = ""
+    val hashArray = ArrayList<Hash>() // 검색한 해시의 사진 url을 저장하는 배열
+    var hashValue:String = "" // 검색된 해시의 사진 url을 저장하는 임시 변수
+    var hash:String = "" // 검색한 hashtag
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,7 +32,45 @@ class DiaryList : Fragment(), AdapterView.OnItemSelectedListener {
     ): View? {
         val inf = inflater.inflate(R.layout.diary_list, container, false)
         root = FirebaseDatabase.getInstance().reference
-            .child("Diary").child(UserModel.uid)
+            .child(UserModel.uid)
+
+        hashRef = root.child("Picture").child("Hash")
+
+        val hashAdapter = HashListAdatper(context, hashArray)
+        inf.diarylistview.adapter = hashAdapter
+        inf.searchBar.setOnQueryTextListener(object: SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.i(tag, "query:"+ query.toString())
+                hash = query.toString()
+                hashRef.addListenerForSingleValueEvent(object: ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.child(hash).exists()) {
+                            hashArray.clear()
+                            val myRef = dataSnapshot.child(hash)
+                            for(i in myRef.children) {
+                                hashValue = i.value.toString()
+                                Log.i(tag, hashValue)
+                                hashArray.add(Hash(hash, hashValue))
+                            }
+                            Log.i(tag, hashArray.toString())
+                        } else {
+                            Toast.makeText(context, "'${hash}' 에 해당하는 태그가 존재하지 않습니다:(", Toast.LENGTH_SHORT).show()
+                        }
+                        hashAdapter.notifyDataSetChanged()
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+                })
+                return false
+            }
+        })
+
         yearSpinner = inf.spinner_year
         yearAdapter = ArrayAdapter.createFromResource(
             context,
@@ -37,17 +82,18 @@ class DiaryList : Fragment(), AdapterView.OnItemSelectedListener {
         }
         yearSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                when(position) {
-                    0 -> yearMonth += 2016
-                    1 -> yearMonth += 2017
-                    2 -> yearMonth += 2018
-                    3 -> yearMonth += 2019
-                    4 -> yearMonth += 2020
+                when (position) {
+                    0 -> yearMonth = "2016"
+                    1 -> yearMonth = "2016"
+                    2 -> yearMonth = "2017"
+                    3 -> yearMonth = "2018"
+                    4 -> yearMonth = "2019"
+                    5 -> yearMonth = "2020"
                 }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-
+                yearMonth = "20161"
             }
         }
 
@@ -61,12 +107,18 @@ class DiaryList : Fragment(), AdapterView.OnItemSelectedListener {
             monthSpinner.adapter = adapter
         }
         monthSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                yearMonth += position
+                Toast.makeText(context, yearMonth, Toast.LENGTH_LONG).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-
+                yearMonth = "20161"
             }
         }
         return inf
--- MyDiary/app/src/main/java/com/example/mydiary/WriteDiary.kt
+++ MyDiary/app/src/main/java/com/example/mydiary/WriteDiary.kt
@@ -1,50 +1,52 @@
 package com.example.mydiary
 
 import android.app.Activity
-
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import com.google.android.gms.tasks.OnSuccessListener
-
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.viewpager.widget.ViewPager
-
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
+import com.example.mydiary.ui.main.GlideApp
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-
 import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.add_schedule.*
 import kotlinx.android.synthetic.main.content_diary.*
-
+import kotlinx.android.synthetic.main.fragment.*
 import java.lang.Exception
-
 
 data class Diary(
     var title : String = "",
-    var contents : String = ""
+    var contents : String = "",
+    var url : String = ""
 )
 
-class WriteDiary: AppCompatActivity() {
+class WriteDiary : AppCompatActivity() {
     private val adapter by lazy { SlideAdapter(supportFragmentManager) }
-    var file : Uri? = null
-    lateinit var tagRef : StorageReference
-
+    var file: Uri? = null
+    var tagRef: StorageReference = FirebaseStorage.getInstance().getReference()
+    lateinit var url :FirebaseDatabase
     var date = ""
-    var yearMonth:String = ""
+    var yearMonth: String = ""
     var day: String = ""
-    var title:String = ""
-    var content:String = ""
-    lateinit var root:DatabaseReference
-    lateinit var diaryRef : DatabaseReference
+    var title: String = ""
+    var content: String = ""
+    var filename = "" // 파이어베이스 storage 에 이미지 저장
+    lateinit var root: DatabaseReference
+    var imageUrl = ""
+    lateinit var diaryRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,9 +55,11 @@ class WriteDiary: AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
         }
+
         var vp = findViewById<ViewPager>(R.id.viewPager)
-        vp.adapter = PutPicture@ adapter
+        vp.adapter = this.adapter
         vp.offscreenPageLimit = 2
+
 
         root = FirebaseDatabase.getInstance().reference.child(UserModel.uid)
         diaryRef = root.child("Diary")
@@ -63,26 +67,37 @@ class WriteDiary: AppCompatActivity() {
         var toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        if(intent.hasExtra("date")) {
+        if (intent.hasExtra("date")) {
             date = intent.getStringExtra("date") // 2019/11/18 형식
             yearMonth = date.split("/")[0] + date.split("/")[1]
             day = date.split("/")[2]
             supportActionBar?.title = date // 날짜를 toolbar의 타이틀로 넣는다
             diaryRef = diaryRef.child(yearMonth)
-            val listener = object : ValueEventListener {
-                override fun onCancelled(databaseError: DatabaseError) {
 
-                }
+            val listener = object : ValueEventListener {
+                override fun onCancelled(databaseError: DatabaseError) {}
 
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    if(dataSnapshot.hasChild(day)) {
-                        title = dataSnapshot.child(day).child("title").value.toString()
-                        content = dataSnapshot.child(day).child("contents").value.toString()
-
+                    if (dataSnapshot.hasChild(day)) {
+                        if(dataSnapshot.child(day).child("title").value != null) {
+                            title = dataSnapshot.child(day).child("title").value.toString()
+                            content = dataSnapshot.child(day).child("contents").value.toString()
+                        } else {
+                            title = ""
+                            content = ""
+                        }
                         editTitle.text.clear()
                         textContent.text.clear()
                         editTitle.text.append(title)
                         textContent.text.append(content)
+
+
+                        val imageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://mydiary-1121.appspot.com")
+                        imageUrl = dataSnapshot.child(day).child("url").value.toString()
+                        if(!imageUrl.equals("")) {
+                            val imageRefChild = imageRef.child(imageUrl)
+                            GlideApp.with(this@WriteDiary).load(imageRefChild).into(fragmentImage)
+                        }
                     } else {
                         editTitle.text.append("")
                         textContent.text.append("")
@@ -93,7 +108,7 @@ class WriteDiary: AppCompatActivity() {
         }
 
         var fab = findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener{moveToPricture()}
+        fab.setOnClickListener { moveToPricture() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -106,12 +121,11 @@ class WriteDiary: AppCompatActivity() {
         if (item != null) {
             return when (item.itemId) {
                 R.id.save -> {
-                    saveDiary(date)
-                    upload(getDataFile())
+                    saveDiary()
                     true
                 }
                 R.id.delete -> {
-                    deleteDiary(date)
+                    deleteDiary()
                     true
                 }
                 else -> super.onOptionsItemSelected(item)
@@ -121,11 +135,15 @@ class WriteDiary: AppCompatActivity() {
     }
 
 
-
-    fun saveDiary(date:String) {
-        var dataInput = Diary(editTitle.text.toString(), textContent.text.toString()) // title = editTitle의 내용, content = textContent의 내용
+    fun saveDiary() {
         diaryRef = root.child("Diary")
         var dR = diaryRef.child(yearMonth)
+        upload(getDataFile())
+        var dataInput = Diary(
+            editTitle.text.toString(),
+            textContent.text.toString(),
+            filename
+        ) // title = editTitle의 내용, content = textContent의 내용
 
         val store = mutableMapOf(day to dataInput)
         dR.updateChildren(store as Map<String, Any>).addOnCompleteListener { task ->
@@ -139,29 +157,42 @@ class WriteDiary: AppCompatActivity() {
         }
     }
 
-    fun deleteDiary(date:String) {
+    fun deleteDiary() {
         diaryRef = root.child("Diary").child(yearMonth).child(day)
-        diaryRef.removeValue().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Toast.makeText(this@WriteDiary, "삭제가 완료되었습니다:D!!", Toast.LENGTH_LONG).show()
-                val i = Intent(applicationContext, ViewPage::class.java)
-                startActivity(i)
-            } else {
-                Toast.makeText(this@WriteDiary, "삭제가 실패했습니다::(", Toast.LENGTH_LONG).show()
+        diaryRef.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if(dataSnapshot.hasChildren()) {
+                    diaryRef.removeValue().addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this@WriteDiary, "삭제가 완료되었습니다:D!!", Toast.LENGTH_LONG).show()
+                            val i = Intent(applicationContext, ViewPage::class.java)
+                            startActivity(i)
+                        } else {
+                            Toast.makeText(this@WriteDiary, "삭제가 실패했습니다::(", Toast.LENGTH_LONG).show()
+                        }
+                    }// 201911이 루트라고 생각하면 됨.
+                } else {
+                    Toast.makeText(this@WriteDiary, "해당 일자에는 일기가 없습니다.", Toast.LENGTH_SHORT).show()
+                }
             }
-        }// 201911이 루트라고 생각하면 됨.
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        })
+
     }
 
 
-    fun moveToPricture(){
+    fun moveToPricture() {
         imagePick()
     }
 
-    fun setDataFile(file: Uri){
+    fun setDataFile(file: Uri) {
         this.file = file
     }
 
-    fun getDataFile():Uri?{
+    fun getDataFile(): Uri? {
         return file
     }
 
@@ -172,7 +203,6 @@ class WriteDiary: AppCompatActivity() {
         intent.type = "image/*"
         intent.setAction(Intent.ACTION_PICK)
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1)
-
     }
 
     @Override
@@ -180,14 +210,15 @@ class WriteDiary: AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         // 갤러리에서 이미지 받아와서 imageview에 저장 ( 삼성: 다중 선택 불가능 )
+
         if (requestCode == 1) {
             if (resultCode == Activity.RESULT_OK && data != null) {
-
                 try {
                     var file = data.data
                     setDataFile(file)
                     var frag = findViewById<ImageView>(R.id.fragmentImage)
                     //upload(filePath)
+
                     frag.setImageURI(file)
                 } catch (e: Exception) {
                     e.printStackTrace()
@@ -197,38 +228,40 @@ class WriteDiary: AppCompatActivity() {
 
     }
 
-    // 파이어베이스 storage 에 이미지 저장
     fun upload(file: Uri?) {
-        if(file != null) {
-            tagRef =  FirebaseStorage.getInstance().reference
+        if(file == null){
+            Toast.makeText(this, "이미지는 업로드 안됨", Toast.LENGTH_LONG).show()
+        }
+
+        else {
+            Log.i("heepie", file.path + "")
+
             var tmp = file.path.split("/")
-            var filename = tmp[tmp.size - 1]
+            filename = tmp[tmp.size - 1]
 
             var ref: StorageReference =
-                tagRef.child("diaryImage/" + "수정")
+                tagRef.child(filename)
 
             ref.putFile(file).addOnSuccessListener(OnSuccessListener {
                 Toast.makeText(this, "성공했습니다", Toast.LENGTH_LONG).show()
-                putTag(file)
+                putTag(filename)
             })
-        } else
-            Toast.makeText(this, "이미지가 넘어오지 않음. 업로드 불가 !!", Toast.LENGTH_LONG).show()
-
+        }
     }
 
-    var tagRoot: DatabaseReference = FirebaseDatabase.getInstance().reference.child(UserModel.uid)
-    var picture = tagRoot.child("Picture")
+    var tagRoot: DatabaseReference = FirebaseDatabase.getInstance().reference
+    var hashPicture = tagRoot.child("${UserModel.uid}/Picture/Hash")
     var tagMap = mutableMapOf<String, Any>()
 
-    fun putTag(file: Uri){
-        if(!tag.text.equals("")){
-            var tmp = tag.text.trim().split("#")
+    fun putTag(filename: String) {
 
-            for(i in 1..tmp.size-1) {
-                var pictureChild = picture.child(tmp[i])
-                tagMap.put("index", file.toString())
-                pictureChild.updateChildren(tagMap)
+        if (!tag.text.equals("")) {
+            var tmp = tag.text.trim().split("#")
+            for (i in 1..tmp.size - 1) {
+                var hash = hashPicture.child(tmp[i]) // 파이어베이스에 해쉬태그별로 목록 생성 #하늘
+                tagMap.put(filename, filename)
+                hash.updateChildren(tagMap)
             }
-        } else{}
+        } else {}
     }
 }
